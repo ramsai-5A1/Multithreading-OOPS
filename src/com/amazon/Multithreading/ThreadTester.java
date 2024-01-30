88a478fbbef60f81498d337e4944c6f09e05b79f
Ramsai <ramsaicrony@gmail.com>
2024-01-30 09:02:30 +0530

    Introduced lambda functions to invoke thread

--- src/com/amazon/Multithreading/ThreadTester.java
+++ src/com/amazon/Multithreading/ThreadTester.java
@@ -4,17 +4,17 @@ public class ThreadTester {
     public static void main(String[] args) {
         System.out.println("Main is Starting");
 
-        Thread1 thread1 = new Thread1("thread-1");
-        thread1.start();
-
-        Thread2 thread2 = new Thread2("thread-2");
-        thread2.start();
-
         Thread thread3 = new Thread(new Thread3(),"thread-3");
         thread3.start();
 
-        Thread thread4 = new Thread(new Thread4(), "thread-4");
+        Thread thread4 = new Thread(() -> {
+            for(int i = 10; i <= 15; i++) {
+                System.out.println("Thread " + Thread.currentThread().getName() + " " + i);
+            }
+        }, "thread-4");
         thread4.start();
+
+
 
         System.out.println("Main is Exiting");
     }
